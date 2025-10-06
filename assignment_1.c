c7f34cf8ad470fa15893cf95aab584e7cbfc23da
Akshit Nalwaya <119034458+Akshitnalwaya@users.noreply.github.com>
2025-10-06 22:24:03 +0530

    Update assignment_1.c

--- assignment_1.c
+++ assignment_1.c
@@ -6,10 +6,10 @@ int main() {
     int i=0,j=0,num=0;
 
     for(i=0,j=0;s[i]!='\0';i++) {
-        if(!((s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/') && (!i && s[i+1]!='\0' && (s[i+1]>='0' || s[i+1]<='9') && (i && s[i-1]>='0' || s[i-1]<='9')))) {
-            printf("Error: Invalid Expression\n");
-            return 0;
-        }
+        // if(!((s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/') && (!i && s[i+1]!='\0' && (s[i+1]>='0' || s[i+1]<='9') && (i && s[i-1]>='0' || s[i-1]<='9')))) {
+        //     printf("Error: Invalid Expression\n");
+        //     return 0;
+        // }
         if(s[i]==' ')continue;
         if(s[i] >= '0' && s[i] <= '9') {
             num *= 10;
