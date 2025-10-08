ed0caaf962eb395b0725a572f9782275ac619a91
Akshit Nalwaya <119034458+Akshitnalwaya@users.noreply.github.com>
2025-10-08 20:55:49 +0530

    Calculator_assignment_1.c

--- assignment_1.c
+++ assignment_1.c
@@ -24,8 +24,9 @@ int main()
     printf("Enter your expression: ");
     fgets(expression, sizeof(expression), stdin);
     int length = 0;
-    while (expression[length] != '\0')
+    while (expression[length] != '\0') {
         length++;
+    }
 
     if (!isValidExpression(expression, length - 1)) {
         printf("Invalid expression syntax\n");
@@ -44,10 +45,13 @@ void performComputation()
     double firstOperand = popOperand();
     double result = 0;
 
-    if (currentOperator == '+') result = firstOperand + secondOperand;
-    else if (currentOperator == '-') result = firstOperand - secondOperand;
-    else if (currentOperator == '*') result = firstOperand * secondOperand;
-    else if (currentOperator == '/') {
+    if (currentOperator == '+') {
+        result = firstOperand + secondOperand;
+    } else if (currentOperator == '-') {
+        result = firstOperand - secondOperand;
+    } else if (currentOperator == '*') {
+        result = firstOperand * secondOperand;
+    } else if (currentOperator == '/') {
         if (secondOperand == 0) {
             printf("dividing by zero\n");
             exit(1);
@@ -61,8 +65,12 @@ void performComputation()
 
 int getPrecedence(char symbol)
 {
-    if (symbol == '*' || symbol == '/') return 2;
-    if (symbol == '+' || symbol == '-') return 1;
+    if (symbol == '*' || symbol == '/') {
+        return 2;
+    }
+    if (symbol == '+' || symbol == '-') {
+        return 1;
+    }
     return 0;
 }
 
@@ -104,48 +112,60 @@ char popOperator()
 
 char peekOperator()
 {
-    if (topOperator == -1) return '0';
+    if (topOperator == -1) {
+        return '0';
+    }
     return operatorStack[topOperator];
 }
 
 double evaluateExpression(char *expression, int length)
 {
     int index = 0;
-    while (index < length)
-    {
-        if (expression[index] == ' ') { index++; continue; }
+    while (index < length) {
+        if (expression[index] == ' ') {
+            index++;
+            continue;
+        }
 
-        if (isdigit(expression[index]))
-        {
+        if (isdigit(expression[index])) {
             double number = 0;
-            while (index < length && isdigit(expression[index]))
-            {
+            while (index < length && isdigit(expression[index])) {
                 number = number * 10 + (expression[index] - '0');
                 index++;
             }
             pushOperand(number);
-        }
-        else if (expression[index] == '(') pushOperator(expression[index++]);
-        else if (expression[index] == ')') {
-            while (peekOperator() != '(') performComputation();
+        } else if (expression[index] == '(') {
+            pushOperator(expression[index++]);
+        } else if (expression[index] == ')') {
+            while (peekOperator() != '(') {
+                performComputation();
+            }
             popOperator();
             index++;
-        }
-        else if (expression[index] == '+' || expression[index] == '-' || expression[index] == '*' || expression[index] == '/')
-        {
-            if (topOperator == -1) { pushOperator(expression[index++]); continue; }
-            if (getPrecedence(peekOperator()) < getPrecedence(expression[index])) pushOperator(expression[index++]);
-            else {
-                while (getPrecedence(peekOperator()) >= getPrecedence(expression[index])) performComputation();
+        } else if (expression[index] == '+' || expression[index] == '-' ||
+                   expression[index] == '*' || expression[index] == '/') {
+            if (topOperator == -1) {
+                pushOperator(expression[index++]);
+                continue;
+            }
+            if (getPrecedence(peekOperator()) < getPrecedence(expression[index])) {
+                pushOperator(expression[index++]);
+            } else {
+                while (getPrecedence(peekOperator()) >= getPrecedence(expression[index])) {
+                    performComputation();
+                }
                 pushOperator(expression[index++]);
             }
-        }
-        else {
+        } else {
             printf("invalid expression\n");
             return 0;
         }
     }
-    while (topOperator != -1) performComputation();
+
+    while (topOperator != -1) {
+        performComputation();
+    }
+
     return operandStack[topOperand];
 }
 
@@ -154,25 +174,49 @@ int isValidExpression(char *expression, int length)
     int index = 0, parenthesesCount = 0;
     char previousChar = 0;
 
-    while (index < length)
-    {
+    while (index < length) {
         char currentChar = expression[index];
-        if (currentChar == ' ') { index++; continue; }
-        if (!(isdigit(currentChar) || currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/' || currentChar == '(' || currentChar == ')')) return 0;
+        if (currentChar == ' ') {
+            index++;
+            continue;
+        }
+        if (!(isdigit(currentChar) || currentChar == '+' || currentChar == '-' ||
+              currentChar == '*' || currentChar == '/' || currentChar == '(' ||
+              currentChar == ')')) {
+            return 0;
+        }
 
-        if (currentChar == '(') { parenthesesCount++; if (expression[index + 1] == ')') return 0; }
-        else if (currentChar == ')') { parenthesesCount--; if (parenthesesCount < 0) return 0; }
+        if (currentChar == '(') {
+            parenthesesCount++;
+            if (expression[index + 1] == ')') {
+                return 0;
+            }
+        } else if (currentChar == ')') {
+            parenthesesCount--;
+            if (parenthesesCount < 0) {
+                return 0;
+            }
+        }
 
-        if ((currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') && 
-            (previousChar == '+' || previousChar == '-' || previousChar == '*' || previousChar == '/')) return 0;
+        if ((currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') &&
+            (previousChar == '+' || previousChar == '-' || previousChar == '*' || previousChar == '/')) {
+            return 0;
+        }
 
-        if (index == 0 && (currentChar == '+' || currentChar == '*' || currentChar == '/')) return 0;
-        if (index == length - 1 && (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/')) return 0;
+        if (index == 0 && (currentChar == '+' || currentChar == '*' || currentChar == '/')) {
+            return 0;
+        }
+        if (index == length - 1 && (currentChar == '+' || currentChar == '-' ||
+                                    currentChar == '*' || currentChar == '/')) {
+            return 0;
+        }
 
         previousChar = currentChar;
         index++;
     }
 
-    if (parenthesesCount != 0) return 0;
+    if (parenthesesCount != 0) {
+        return 0;
+    }
     return 1;
 }
